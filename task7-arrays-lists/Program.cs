dd05b56bd5dd45c49079b3fe1cc72080306ae042
Apolo <paolacrisnvs@gmail.com>
2025-05-12 18:45:15 -0300

    Add project files.

---
+++ task7-arrays-lists/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numeros = { 5, 10, 20, 30, 12 };
+int soma = 0;
+
+foreach (int numero in numeros)
+{
+    soma += numero;
+}
+
+double media = soma / numeros.Length;
+
+Console.WriteLine("Média: " + media);
+
+Console.WriteLine("\r\nNomes com A");
+List<string> nomes = new List<string> { "Amanda", "Fernanda", "Marcos", "Apolo", "Carlos", "Eduarda" };
+foreach (string nome in nomes)
+{
+    if (nome.StartsWith("A"))
+    {
+        Console.WriteLine(nome);
+    }
+}
+
